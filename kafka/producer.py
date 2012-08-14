4bdc85396de6c8b8671b17497891ca7d905d9cc7
Stephen Holsapple <sholsapp@gmail.com>
2012-08-14 13:39:11 -0700

    Added a BatchProducer that uses a time interval to batch messages. Did some PEP8 formatting. Added a .gitignore file for Python. Added tests for kafka producers.

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,27 +1,39 @@
 import contextlib
 import itertools
 import struct
+import threading
 
 import kafka.io
 import kafka.request_type
 
 class Producer(kafka.io.IO):
-  """ Class for sending data to a `Kafka <http://sna-projects.com/kafka/>`_ broker. """
+  """Class for sending data to a `Kafka <http://sna-projects.com/kafka/>`_ broker."""
 
   PRODUCE_REQUEST_ID = kafka.request_type.PRODUCE
 
   def __init__(self, topic, partition=0, host='localhost', port=9092):
+    """Setup a kafka producer.
+
+      :param topic: The topic to produce to.
+      :param partition: The broker partition to produce to.
+      :param host: The kafka host.
+      :param port: The kafka port.
+
+    """
     kafka.io.IO.__init__(self, host, port)
-
-    self.topic     = topic
+    self.topic = topic
     self.partition = partition
-
     self.connect()
 
   def encode_request(self, messages):
-    """ Encode a sequence of :class:`Message <kafka.message>` objects for sending to the broker. """
+    """Encode a sequence of messages for sending to the broker.
 
-    # encode messages as <LEN: int><MESSAGE_BYTES>
+      :param messages: A sequence of :class:`Message <kafka.message>` objects.
+
+    """
+    # encode messages as::
+    #   <LEN: int>
+    #   <MESSAGE_BYTES>
     encoded = [message.encode() for message in messages]
     lengths = [len(em) for em in encoded]
 
@@ -31,27 +43,84 @@ class Producer(kafka.io.IO):
     # Flatten the two lists to match the format.
     message_set = struct.pack(mformat, *list(itertools.chain.from_iterable(zip(lengths, encoded))))
 
+    # Create the request as::
+    #   <REQUEST_SIZE: int>
+    #   <REQUEST_ID: short>
+    #   <TOPIC: bytes>
+    #   <PARTITION: int>
+    #   <BUFFER_SIZE: int>
+    #   <BUFFER: bytes>
     topic_len = len(self.topic)
-    mset_len  = len(message_set)
-
-    # Create the request as <REQUEST_SIZE: int> <REQUEST_ID: short> <TOPIC: bytes> <PARTITION: int> <BUFFER_SIZE: int> <BUFFER: bytes>
-    pformat   = '>HH%dsii%ds' % (topic_len, mset_len)
-    payload   = struct.pack(pformat, self.PRODUCE_REQUEST_ID, topic_len, self.topic, self.partition, mset_len, message_set)
-
+    mset_len = len(message_set)
+    pformat = '>HH%dsii%ds' % (topic_len, mset_len)
+    payload = struct.pack(pformat, self.PRODUCE_REQUEST_ID, topic_len, self.topic, self.partition, mset_len, message_set)
     return struct.pack('>i%ds' % len(payload), len(payload), payload)
 
   def send(self, messages):
-    """ Send a :class:`Message <kafka.message>` or a sequence of `Messages` to the Kafka server. """
-
+    """Send a :class:`Message <kafka.message>` or a sequence of `Messages` to the Kafka server."""
     if isinstance(messages, kafka.message.Message):
       messages = [messages]
-
     return self.write(self.encode_request(messages))
 
   @contextlib.contextmanager
   def batch(self):
-    """ Send messages with an implict `send`. """
-
+    """Send messages with an implict `send`."""
     messages = []
     yield(messages)
     self.send(messages)
+
+
+class BatchProducer(Producer):
+  """Class for batching messages to a `Kafka <http://sna-projects.com/kafka/>`_ broker with periodic flushing."""
+
+  PRODUCE_REQUEST_ID = kafka.request_type.PRODUCE
+
+  def __init__(self, topic, batch_interval, partition=0, host='localhost', port=9092):
+    """Setup a batch kafka producer.
+
+      :param topic: The topic to produce to.
+      :param batch_interval: The amount of time (in seconds) to wait for messages before sending.
+      :param partition: The broker partition to produce to.
+      :param host: The kafka host.
+      :param port: The kafka port.
+
+    """
+    Producer.__init__(self, topic, partition=partition, host=host, port=port)
+    self.batch_interval = batch_interval
+    self._message_queue = []
+    self.event = threading.Event()
+    self.lock = threading.Lock()
+    self.timer = threading.Thread(target=self._interval_timer)
+    self.connect()
+    self.timer.start()
+
+  def _interval_timer(self):
+    """Flush the message queue every `batch_interval` seconds."""
+    while not self.event.is_set():
+      self.flush()
+      self.event.wait(self.batch_interval)
+
+  def enqueue(self, message):
+    """Enqueue a message in the queue.
+
+      .. note:: These messages are implicitly sent every `batch_interval` seconds.
+
+      :param message: The message to queue for sending at next send interval.
+
+    """
+    with self.lock:
+      self._message_queue.append(message)
+
+  def flush(self):
+    """Send all messages in the queue now."""
+    with self.lock:
+      if len(self._message_queue) > 0:
+        self.send(self._message_queue)
+        # Reset the queue
+        del self._message_queue[:]
+
+  def close(self):
+    """Shutdown the timer thread and flush the message queue."""
+    self.event.set()
+    self.flush()
+    self.timer.join()
---
+++ tests/test_producers.py
@@ -0,0 +1,45 @@
+import socket
+import time
+import unittest
+from StringIO import StringIO
+
+from fudge import patch
+
+from kafka import message
+from kafka import producer
+
+
+class TestProducers(unittest.TestCase):
+  """Tests kafka producers."""
+
+  @patch('socket.socket.connect', 'kafka.io.IO.write')
+  def test_producer(self, fake_connect, fake_write):
+    """Test the basic kafka producer."""
+    expected_msg = ('\x00\x00\x00$\x00\x00\x00\tFakeTopic\x00\x00\x00\x00'
+                    '\x00\x00\x00\x0f\x00\x00\x00\x0b\x00\xcf\x02\xbb\\abc123')
+    fake_socket = fake_connect.expects_call().with_args(('localhost', 1234)).returns_fake()
+    fake_write.expects_call().with_args(expected_msg)
+    p = producer.Producer('FakeTopic', host='localhost', port=1234)
+    p.send(message.Message('abc123'))
+
+
+  @patch('socket.socket.connect', 'kafka.io.IO.write')
+  def test_batch_producer(self, fake_connect, fake_write):
+    """Test the batch kafka producer with a batch interval."""
+    # Keep this small to keep test cases quick!
+    batch_interval = 0.5
+    expected_msg_one = ('\x00\x00\x00B\x00\x00\x00\tFakeTopic\x00\x00\x00\x00\x00'
+                        '\x00\x00-\x00\x00\x00\x0b\x00\xcf\x02\xbb\\abc123\x00\x00'
+                        '\x00\x0b\x00#\xd1\xa6ndef456\x00\x00\x00\x0b\x00"\xe1\xdd'
+                        '\xa2ghi789')
+    expected_msg_two = ('\x00\x00\x00$\x00\x00\x00\tFakeTopic\x00\x00\x00\x00\x00'
+                        '\x00\x00\x0f\x00\x00\x00\x0b\x00\xf0\xb69\xb8jkl123')
+    fake_socket = fake_connect.expects_call().with_args(('localhost', 1234)).returns_fake()
+    fake_write.expects_call().with_args(expected_msg_one).next_call().with_args(expected_msg_two)
+    p = producer.BatchProducer('FakeTopic', batch_interval, host='localhost', port=1234)
+    p.enqueue(message.Message('abc123'))
+    p.enqueue(message.Message('def456'))
+    p.enqueue(message.Message('ghi789'))
+    time.sleep(batch_interval)
+    p.enqueue(message.Message('jkl123'))
+    p.close()
